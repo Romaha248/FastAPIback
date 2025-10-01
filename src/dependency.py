dcb29e5ea69c0ec037e7b0213e0ac780898b98fb
Roman <romanbezverkhyi5@gmail.com>
2025-10-01 19:04:52 +0300

    change db depend

--- src/dependency.py
+++ src/dependency.py
@@ -7,8 +7,10 @@ from typing import Annotated
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
-        async with session.begin():
+        try:
             yield session
+        finally:
+            await session.close()
 
 
 DbSession = Annotated[AsyncSession, Depends(get_db)]
