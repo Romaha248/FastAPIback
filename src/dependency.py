26a3aac99d0d2e9b0954c4063565e097724765ba
Roman <romanbezverkhyi5@gmail.com>
2025-10-01 18:21:53 +0300

    change db dependency

--- src/dependency.py
+++ src/dependency.py
@@ -4,14 +4,22 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 
 
+# async def get_db():
+#     async with AsyncSessionLocal() as session:
+#         try:
+#             yield session
+#             await session.commit()
+#         except:
+#             await session.rollback()
+#             raise
+#         finally:
+#             await session.close()
+
+
 async def get_db():
     async with AsyncSessionLocal() as session:
         try:
             yield session
-            await session.commit()
-        except:
-            await session.rollback()
-            raise
         finally:
             await session.close()
 
