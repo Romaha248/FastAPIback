c9dddf0708a76589a234b1164f1d03543a5fb812
Roman <romanbezverkhyi5@gmail.com>
2025-10-01 22:40:26 +0300

    change db connection

--- src/database/dbcore.py
+++ src/database/dbcore.py
@@ -1,5 +1,38 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+# from sqlalchemy.orm import declarative_base
+# import os
+# import re
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# DATABASE_URL = os.getenv("POSTGRES_URL")
+
+# if not DATABASE_URL:
+#     raise ValueError("POSTGRES_URL is not set in environment")
+
+# DATABASE_URL = re.sub(r"^postgresql:", "postgresql+asyncpg:", DATABASE_URL)
+
+# engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+# AsyncSessionLocal = async_sessionmaker(
+#     bind=engine, class_=AsyncSession, expire_on_commit=False, autocommit=False
+# )
+
+# Base = declarative_base()
+
+# from src.entities import users, todos
+
+import contextlib
+from typing import Any, AsyncIterator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncConnection,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
 import os
 import re
 from dotenv import load_dotenv
@@ -13,12 +46,58 @@ if not DATABASE_URL:
 
 DATABASE_URL = re.sub(r"^postgresql:", "postgresql+asyncpg:", DATABASE_URL)
 
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
-)
+class Base(DeclarativeBase):
+    # https://docs.sqlalchemy.org/en/14/orm/extensions/asyncio.html#preventing-implicit-io-when-using-asyncsession
+    __mapper_args__ = {"eager_defaults": True}
 
-Base = declarative_base()
 
-from src.entities import users, todos
+# Heavily inspired by https://praciano.com.br/fastapi-and-async-sqlalchemy-20-with-pytest-done-right.html
+class DatabaseSessionManager:
+    def __init__(self, host: str, engine_kwargs: dict[str, Any] = {}):
+        self._engine = create_async_engine(host, **engine_kwargs)
+        self._sessionmaker = async_sessionmaker(
+            autocommit=False, bind=self._engine, expire_on_commit=False
+        )
+
+    async def close(self):
+        if self._engine is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+        await self._engine.dispose()
+
+        self._engine = None
+        self._sessionmaker = None
+
+    @contextlib.asynccontextmanager
+    async def connect(self) -> AsyncIterator[AsyncConnection]:
+        if self._engine is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+
+        async with self._engine.begin() as connection:
+            try:
+                yield connection
+            except Exception:
+                await connection.rollback()
+                raise
+
+    @contextlib.asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncSession]:
+        if self._sessionmaker is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+
+        session = self._sessionmaker()
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+
+sessionmanager = DatabaseSessionManager(DATABASE_URL, {"echo": True})
+
+
+async def get_db_session():
+    async with sessionmanager.session() as session:
+        yield session
--- src/dependency.py
+++ src/dependency.py
@@ -1,20 +1,28 @@
-from typing import AsyncGenerator
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.database.dbcore import AsyncSessionLocal
+# from typing import AsyncGenerator
+# from fastapi import Depends
+# from sqlalchemy.ext.asyncio import AsyncSession
+# from src.database.dbcore import AsyncSessionLocal
+# from typing import Annotated
+
+
+# async def get_db() -> AsyncGenerator[AsyncSession, None]:
+#     async with AsyncSessionLocal() as session:
+#         try:
+#             yield session
+#             await session.commit()
+#         except:
+#             await session.rollback()
+#             raise
+#         finally:
+#             await session.close()
+
+
+# DbSession = Annotated[AsyncSession, Depends(get_db)]
+
 from typing import Annotated
 
+from src.database.dbcore import get_db_session
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSessionLocal() as session:
-        try:
-            yield session
-            await session.commit()
-        except:
-            await session.rollback()
-            raise
-        finally:
-            session.close()
-
-
-DbSession = Annotated[AsyncSession, Depends(get_db)]
+DBSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
