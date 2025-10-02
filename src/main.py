3c25c73f7b9d845b50e18be729203f2998d5eac3
Roman <romanbezverkhyi5@gmail.com>
2025-10-02 13:39:05 +0300

    create cors do be able connect to frontend

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.database.dbcore import Base, engine
 from src.entities import users, todos
 from src.api import register_routes
@@ -6,5 +7,15 @@ from src.api import register_routes
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 register_routes(app)
